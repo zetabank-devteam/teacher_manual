f0e33b0b8cce856713ccbc649edf4136c79a172b
Big-Arm <108450807+Big-Arm@users.noreply.github.com>
2022-12-26 15:26:23 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,12 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_sidebars = {
+    '**': [
+        'globaltoc.html',
+    ]
+}
+
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
