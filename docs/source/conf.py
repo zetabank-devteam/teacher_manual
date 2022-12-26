90447d425702eb98abef7fdc45d0e993cd95613f
Big-Arm <108450807+Big-Arm@users.noreply.github.com>
2022-12-26 12:53:54 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,10 +31,10 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
-html: 
-    use_issues_button: true
-    use_repository_button: true
-
+html = [
+    'use_issues_button': true,
+    'use_repository_button': true,
+]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
